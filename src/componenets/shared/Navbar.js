4ca352e0f5803693956eb416e35c19d75a030bd9
parastoopourazizi <napourazizi@gmail.com>
2024-08-27 14:44:07 +0330

    add navbar & css navbar

---
+++ src/componenets/shared/Navbar.js
@@ -0,0 +1,16 @@
+import styles from './Navbar.module.css'
+import {Link} from 'react-router-dom'
+function Navbar(){
+    return(
+        <div className={styles.mainContainer}>
+            <div className={styles.container}>
+                <Link>
+                </Link>
+
+            </div>
+
+        </div>
+    )
+
+}
+export default Navbar
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Navbar from './componenets/shared/Navbar';
 
 
 
@@ -7,7 +8,8 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   
+    <Navbar />
+ 
   </React.StrictMode>
 );
 
