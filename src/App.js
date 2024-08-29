deced5e3ef90f8b7b399a95f2786e2926a669893
parastoopourazizi <napourazizi@gmail.com>
2024-08-29 13:14:23 +0330

    add app & add icon navbar

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Navbar from "./componenets/shared/Navbar"
+
+function App(){
+    return(
+        <Navbar />
+    )
+
+}
+export default App
--- src/componenets/shared/Navbar.js
+++ src/componenets/shared/Navbar.js
@@ -1,16 +1,22 @@
-import styles from './Navbar.module.css'
-import {Link} from 'react-router-dom'
-function Navbar(){
-    return(
-        <div className={styles.mainContainer}>
-            <div className={styles.container}>
-                <Link>
-                </Link>
+import styles from "./Navbar.module.css";
+import shopCart from "../../assets/icons/shop.svg"
+
+function Navbar() {
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.container}>
+        <div className="styles.productLink" to="/">
+          product
+        </div>
+        <div className="styles.iconContainer">
+            <div to="/">
+            <img src={shopCart} alt="shop" />
 
             </div>
 
         </div>
-    )
-
+      </div>
+    </div>
+  );
 }
-export default Navbar
+export default Navbar;
